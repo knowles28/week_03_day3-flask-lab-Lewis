6e38c9f61fe6a0c39294ef41a248a2d585b035df
Jack Knowles <jackmhknowles@gmail.com>
2022-07-06 14:50:25 +0100

    second commit

--- controllers/order_controller.py
+++ controllers/order_controller.py
@@ -5,3 +5,7 @@ from models.customer_orders import customer_orders
 @app.route('/')
 def index():
     return render_template('index.html', all_orders=customer_orders)
+
+@app.route('/orders/<index>')
+def order():
+    return render_template("order.html", )
