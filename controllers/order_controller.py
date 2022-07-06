0a5c042dbf4a3bc7b3896ccaf048270348ac4e83
Jack Knowles <jackmhknowles@gmail.com>
2022-07-06 16:10:53 +0100

    added order html fix with help from Skye

--- controllers/order_controller.py
+++ controllers/order_controller.py
@@ -4,13 +4,12 @@ from app import app
 from models.customer_orders import customer_orders
 
 
-@app.route('/')
+@app.route('/orders')
 def index():
     return render_template('index.html', all_orders=customer_orders)
 
 @app.route('/orders/<int:index>')
 def order(index):
-    link_order = customer_orders[index]
-    return render_template('order.html', link_order=link_order, all_orders=customer_orders)
+    return render_template('order.html', order=customer_orders[index])
 
 
