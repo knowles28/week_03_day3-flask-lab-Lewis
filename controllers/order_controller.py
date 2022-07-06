6c78ef0f0cee8e53e509e3d5632b92cf4c9a8373
Jack Knowles <jackmhknowles@gmail.com>
2022-07-06 15:35:56 +0100

    bloody comments

--- controllers/order_controller.py
+++ controllers/order_controller.py
@@ -1,11 +1,16 @@
+from os import link
 from flask import render_template
 from app import app
 from models.customer_orders import customer_orders
+
 
 @app.route('/')
 def index():
     return render_template('index.html', all_orders=customer_orders)
 
-@app.route('/orders/<index>')
-def order():
-    return render_template("order.html", )
+@app.route('/orders/<int:index>')
+def order(index):
+    link_order = customer_orders[index]
+    return render_template('order.html', link_order=link_order, all_orders=customer_orders)
+
+
